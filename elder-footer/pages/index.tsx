f8a53dadcd518368019dd3a2f84b3ab709e0ad9c
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-08-24 14:45:13 +0900

    footer Layout_Ver.01

--- elder-footer/pages/index.tsx
+++ elder-footer/pages/index.tsx
@@ -1,36 +1,49 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 //https://tailwindcss.com/docs/guides/nextjs 참고
 //https://geonlee.tistory.com/235 참고
+//https://velog.io/@zero_mountain/NextNext.js%EC%97%90%EC%84%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0 로컬 이미지 삽입 참고
+//https://www.youtube.com/watch?v=5xDVP02g9WQ 폰트 변경 참고
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-
-<h1 className="text-3xl font-bold underline">
-     tail wind 배우기!!
-    </h1>
-    <h1>tail wind 배우기!!</h1>
-     
-      <footer className={styles.footer}>
-        <h3>여기서부터 footer입니다</h3>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+    <div>
+      <div className="bg-white w-[1300px] h-[350px]">
+        <h2>body</h2>
+      </div>
+      <footer className="flex w-[1300px] text-[12px] place-content-center border border-white-500 leading-[14.52px] opacity-70 font-inter">
+        <div className="flex w-[1200px] justify-between my-[50px]">
+          <div className="w-[423px]">
+            <div>
+              <div className="mb-2">
+                <img src="/img/symbol.svg" alt="symbol" />
+              </div>
+              <div>
+                The world’s first and largest digital marketplace for crypto
+                collectibles and non-fungible tokens (NFTs). Buy, sell, and
+                discover exclusive digital items.
+              </div>
+            </div>
+            <div className="mt-4">@ 2022 - 2022 Mosaic Square Labs , Inc</div>
+          </div>
+          <div className="flex flex-col self-center">
+            <div className="flex flex justify-end p-1.5 mb-6">
+              <img src="/img/twitter.svg" alt="twitter" className="mr-[17px]" />
+              <img src="/img/instagram.svg" alt="instagram" />
+            </div>
+            <div className="flex flex-row">
+              <h5 className="mr-4 opacity-30">Privacy Policy</h5>
+              <h5 className="mr-4 opacity-30">Terms of Service</h5>
+              <h5>Help Center</h5>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
